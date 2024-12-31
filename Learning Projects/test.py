7d97625bcf5f687b3f12827afc97879a00c713d1
mahdi.karimisabah <mk.sabah@gmail.com>
2024-12-31 12:44:57 +0100

    update

--- Learning Projects/test.py
+++ Learning Projects/test.py
@@ -9,6 +9,7 @@ window = Tk()
 window.title("Digital Clock")
 window.geometry("800x500")
 
+
 def get_user_location():
     g = geocoder.ip("me")
     latitude, longitude = g.latlng
@@ -34,14 +35,43 @@ def myTime():
     myLabel.config(text=myText)
     myLabel2.config(text=myText2)
 
+    if selected_timzone.get() != "":
+        timezone = pytz.timezone(selected_timzone.get())
+        tz_time = datetime.now(timezone)
+        tz_hour = tz_time.strftime("%I")
+        tz_minute = tz_time.strftime("%M")
+        tz_second = tz_time.strftime("%S")
+        tz_am_pm = tz_time.strftime("%p")
+        tz_day = tz_time.strftime("%A")
+        tz_date = tz_time.strftime("%Y-%m-%d")
+
+        tz_text = f"{tz_hour}:{tz_minute}:{tz_second} {tz_am_pm}\n{tz_day}, {tz_date}\n{selected_timzone.get()}"
+        tzLabel.config(text=tz_text)
+    else:
+        tzLabel.config(text="")
+
+
     myLabel.after(1000, myTime)
 
 
 myLabel = Label(window, text="", font=("Calibri", 72, "bold"), fg="Yellow", bg="Gray")
 myLabel.pack()
 
-myLabel2 = Label(window, text="MK", font=("calibri", 38, "bold"), fg="Yellow", bg="Purple")
-myLabel2.pack()
+myLabel2 = Label(window, text="", font=("calibri", 38, "bold"), fg="Yellow", bg="Purple")
+myLabel2.pack(pady=10)
+
+tzLabel =Label(window, text="", font=("Calibri", 24, "bold"), fg="White", bg="Blue")
+tzLabel.pack(pady=10)
+
+search_entry = Entry(window, font=("Calibri", 14))
+search_entry.pack(pady=10)
+
+frame = Frame(window)
+frame.pack()
+
+selected_timzone = StringVar()
+selected_timzone.set("")
+
 
 myTime()
 window.mainloop()
