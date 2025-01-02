8a56f3583202723e11d1c2829f029009bf610609
mahdi.karimisabah <mk.sabah@gmail.com>
2025-01-02 19:35:31 +0100

    update

--- Tik_Tak_Tue.py
+++ Tik_Tak_Tue.py
@@ -1,15 +1,31 @@
-import tkinter as Tk 
+import tkinter as tk
 from tkinter import messagebox 
 
-class TicTakTueGUI:
+class TicTakToeGUI:
     def __init__(self, board_size=3):
         self.boars_size = board_size
         self.window = tk.Tk()
-        self.window.title('Tic Tak Tue')
+        self.window.title('Tic Tak Toe')
         self.board = [[' ' for _ in range(self.boars_size)] for _ in range(self.boars_size)]
         self.current_player = 'X'
-        self.creat_board_buttons()
+        self.create_board_buttons()
 
     def create_board_buttons(self):
-        self.buttons = [[tk.button(self.window, text= ' ', font='Arial', ))]]
+        self.buttons = [[tk.Button(self.window, text=' ', font=('Arial', 24), width=3, height=1,
+                                   command= lambda row=row, col=col: self.make_move(row, col)) for col in range(self.boars_size)]
+                                   for row in range(self.boars_size)]
+        for row in range(self.boars_size):
+            for col in range(self.boars_size):
+                self.buttons[row][col].grid(row=row, col=col)
+
+    def make_move(self, row, col):
+        pass
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    board_size = int(input("Enter the size of the board(e.g. , 3 for 3x3, 4 for 4x4, etc.): "))
+    tic_tac_toe = TicTakToeGUI(board_size)
+    tic_tac_toe.run()
 
